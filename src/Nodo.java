9f448a6aee150d98d071ff991d4d1e83afdf800a
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-31 20:23:25 -0500

    constructor de la clase nodo

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,7 +1,12 @@
 public class Nodo {
-    //atributos
+    // atributos
     private int dato;
     private Nodo siguiente;
     private Nodo anterior;
-    
+
+    // constructor
+    public Nodo(int elemento) {
+        dato = elemento;
+        siguiente = anterior = null;
+    }
 }
