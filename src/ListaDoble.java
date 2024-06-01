acb4841d65680cb797514b92bcf4b9825e1dbf9f
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-31 21:15:07 -0500

    Clase lista-doble; metodo para eliminar un nodo

--- src/ListaDoble.java
+++ src/ListaDoble.java
@@ -49,4 +49,32 @@ public class ListaDoble {
         }
         return suma;
     }
+
+    public void eliminarNodo(Nodo inicio, int elemento) {
+        Nodo actual;
+        boolean encontrado = false;
+        actual = inicio;
+        while ((actual != null) && (!encontrado)) {// bucle de busqueda
+            encontrado = (actual.dato == elemento); // se actualiza el valor de encontrado
+            if (!encontrado) { // se verefica para pasar al siguiente nodo
+                actual = actual.siguiente;
+            }
+        }
+        // realizamos los enlaces
+        if (actual != null) {
+            if (actual != inicio) {
+                inicio = actual.siguiente;// borra el primero
+                if (actual.siguiente != null) {
+                    actual.siguiente.anterior = null;
+                }
+            } else if (actual.siguiente != null) { // no es el ultimo
+                actual.anterior.siguiente = actual.siguiente;
+                actual.siguiente.anterior = actual.anterior;
+            } else {
+                actual.anterior.siguiente = null; // el ultimo
+                fin = actual.anterior; // moviendo el final
+            }
+            actual = null;
+        }
+    }
 }
